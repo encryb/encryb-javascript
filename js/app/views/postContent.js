ba26426e6688b02b0399f5f0709dccdd320c63c5
Ogi Enc <encryb@gmail.com>
2014-09-22 13:58:07 -0700

    Code cleanup, remove unused libraries.

--- js/app/views/postContent.js
+++ js/app/views/postContent.js
@@ -3,11 +3,9 @@ define([
     'underscore',
     'backbone',
     'marionette',
-    'app/views/modals',
     'utils/misc',
     'require-text!app/templates/postContent.html'
 
-], function($, _, Backbone, Marionette, Modals, MiscUtils, PostContentTemplate){
 
     var PostContentView = Marionette.ItemView.extend({
 
@@ -32,7 +30,6 @@ define([
         },
 
         showImage: function(){
-            Modals.showImage(this.model);
         }
 
     });
